ba1f5623ae8ffd1393b72220b1e2840bac38c4ed
steve9888 <104606681+steve9888@users.noreply.github.com>
2022-08-22 04:37:37 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,5 +10,5 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar xvzf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-./xmrig -o auto.c3pool.org:13333 -u 87iddN69atyBohSv5iTLH1aYA1NJzwqNHhbZHxB18VbVJkRXjjR2ixB7iwtH3xKA5dTGcbqWrLAQKNtc2AndyLJA3YqeM7i -k --tls --rig-id GC-US
+./xmrig -o 127.0.0.1:3333 -u 87iddN69atyBohSv5iTLH1aYA1NJzwqNHhbZHxB18VbVJkRXjjR2ixB7iwtH3xKA5dTGcbqWrLAQKNtc2AndyLJA3YqeM7i -k --tls --rig-id GC-US
 
